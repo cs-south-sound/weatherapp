7043f6c42bbc9141bf164a31e7301e98129c5586
Randal Kamradt <randysr@kamradtfamily.net>
2015-09-26 11:11:05 -0700

    fixed tests

--- test/test.js
+++ test/test.js
@@ -2,35 +2,17 @@ test("test converting Kelvin to Fahrenheit", function() {
   equal(convert.KtoF(1),-458);
 });
 test("test converting Kelvin to Celsius", function() {
-  equal(convert.KtoF(1),-458);
+  equal(convert.KtoC(1),274);
 });
 test("test converting Celsius to Fahrenheit", function() {
-  equal(convert.KtoF(1),-458);
+  equal(convert.CtoF(1),34);
 });
 test("test converting Celsius to Kelvin", function() {
-  equal(convert.KtoF(1),-458);
+  equal(convert.CtoK(1),-272);
 });
-test("test converting Kelving to Fahrenheit", function() {
-  equal(convert.KtoF(1),-458);
+test("test converting Fahrenheit to Kelvin", function() {
+  equal(convert.FtoK(1),256);
 });
-test("test converting Kelving to Celsius", function() {
-  equal(convert.KtoF(1),-458);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.CtoF(NaN), NaN);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.CtoK(NaN), NaN);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.FtoC(NaN), NaN);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.FtoK(NaN), NaN);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.KtoC(NaN), NaN);
-});
-test("test temperature converts with bad from", function() {
-  equal(convert.KtoF(NaN), NaN);
+test("test converting Fahrenheit to Celsius", function() {
+  equal(convert.FtoC(1),-17);
 });
